4d35bf883ad4e73a904ee2401b74a31242b595c1
Cesar Parent <cesar@cesarparent.com>
2016-02-13 23:40:09 +0000

    Basic HTTP server script

---
+++ server/http/server.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Request.php - contains information about the request that was sent to the
+ * http server.
+ * 
+ * @version     1.0a1
+ * @author      Cesar Parent <cesar@cesarparent.com>
+ * @package     http
+ */
+namespace http;
+require_once(__DIR__."/request.php");
+require_once(__DIR__."/response.php");
+
+function createServer(callable $handler) {
+    return new server($handler);
+}
+
+
+class server {
+    
+    private $_request;
+    private $_response;
+    
+    private $_handler;
+    
+    public function __construct(callable $handler) {
+        $this->_handler = $handler;
+        $this->_request = new Request();
+        $this->_response = new Response();
+    }
+    
+    public function start() {
+        call_user_func($this->_handler, $this->_request, $this->_response);
+        $this->_send();
+    }
+    
+    private function _send() {
+        $body = $this->_response->body();
+        $len = strlen(utf8_decode($body));
+        header($this->_response->status());
+        header("Content-Length: ".$len);
+        foreach($this->_response->headers() as $k => $v) {
+            header($k.": ".$v);
+        }
+        echo $body;
+    }
+}
