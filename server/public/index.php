b2d758c9e165847e98a7ea76a7a971d1974800dc
Amy Parent <amy@amyparent.com>
2016-02-13 23:40:09 +0000

    Basic HTTP server script

--- server/public/index.php
+++ server/public/index.php
@@ -3,40 +3,13 @@
  * Simple server script for a single-user note taking application
  * Calls function from Controller.php
  */
-namespace CesarParent;
-require_once(__DIR__."/../REST/Engine.php");
-require_once(__DIR__."/../ViewModel.php");
-use CesarParent\REST;
-use CesarParent\ViewModel;
 
-$app = new REST\Engine(__DIR__."/../config.json");
-$vm = new ViewModel\ViewModel($app);
+require_once(__DIR__."/../http/server.php");
 
-$app->route('GET', '/', function() use($app) {
-    return REST\Response::success(200);
-});
+error_reporting(E_ALL);
 
-$app->route('GET', '/notes', function() use($vm) {
-    return $vm->get_notes();
-});
-
-$app->route('POST', '/notes', function() use($vm) {
-    return $vm->create_notes();
-});
-
-$app->route('GET', '/notes/([0-9]+)', function($id) use($vm) {
-    return $vm->get_note($id);
-});
-
-$app->route('PUT', '/notes/([0-9]+)', function($id) use($vm) {
-    return $vm->edit_note($id);
-});
-
-$app->route('DELETE', '/notes/([0-9]+)', function($id) use($vm) {
-    if(!$vm->authenticate()) {
-        return REST\Response::fail(401);
-    }
-    return $vm->delete_note($id);
-});
-
-$app->execute($_REQUEST['resource']);
+(new http\server(function($req, $res) {
+    $res->setHeader("Content-Type", "text/plain");
+    $res->setStatusCode(404);
+    $res->send("Hello\n");
+}))->start();
