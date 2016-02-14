918773b95642a33a6859ec65f65b64d6a21c81d5
Cesar Parent <cesar@cesarparent.com>
2016-02-14 00:09:59 +0000

    Adds a basic class autoloader

--- server/public/index.php
+++ server/public/index.php
@@ -4,13 +4,13 @@
  * Calls functions from Controller.php
  */
 
-require_once(__DIR__."/../http/server.php");
+require_once(__DIR__."/../loader.php");
 
 error_reporting(E_ALL);
 
-http\createServer(function($req, $res) {
+(new http\server(function($req, $res) {
     $res->setHeader("Content-Type", "text/plain");
     $res->setStatusCode(200);
     $res->send($req->query("test", -1)."\n");
     $res->send("Hello\n");
-})->start();
+}))->start();
