c3615fb217b4d49d33c1741eaeecd557c6fa1b2b
Cesar Parent <cesar@cesarparent.com>
2016-02-14 00:01:12 +0000

    Adds output filter, req. URL & body

--- server/public/index.php
+++ server/public/index.php
@@ -1,15 +1,16 @@
 <?php
 /**
  * Simple server script for a single-user note taking application
- * Calls function from Controller.php
+ * Calls functions from Controller.php
  */
 
 require_once(__DIR__."/../http/server.php");
 
 error_reporting(E_ALL);
 
-(new http\server(function($req, $res) {
+http\createServer(function($req, $res) {
     $res->setHeader("Content-Type", "text/plain");
-    $res->setStatusCode(404);
+    $res->setStatusCode(200);
+    $res->send($req->query("test", -1)."\n");
     $res->send("Hello\n");
-}))->start();
+})->start();
