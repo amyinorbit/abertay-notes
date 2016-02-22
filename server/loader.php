48f251fc9efa8e6c6027e20dedfc9b01ba750e1f
Amy Parent <amy@amyparent.com>
2016-02-22 16:23:52 +0000

    Moved autoloaded classes to /server/classes

--- server/loader.php
+++ server/loader.php
@@ -1,5 +1,5 @@
 <?php
 spl_autoload_register(function($className) {
     $class = strtolower(trim($className, "\\"));
-    require_once(__DIR__."/".str_replace("\\", "/", $class).".php");
+    require_once(__DIR__."/classes/".str_replace("\\", "/", $class).".php");
 });
