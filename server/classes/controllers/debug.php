2e84ccac631eba347b0faa601ff46dbfa9e9ca92
Amy Parent <amy@amyparent.com>
2016-03-22 12:53:54 +0000

    added user debug capability

--- server/classes/controllers/debug.php
+++ server/classes/controllers/debug.php
@@ -34,39 +34,59 @@ EOT;
 
     public static function Display($req, $res, $email) {
         
-        
-        
-        ob_start();
-        echo self::$header;
-        echo "Hello";
-        echo self::$footer;
+        self::Start();
         
         $user = self::GetUser($email);
         if(is_null($user)) {
             return self::Finish($res);
         }
         self::PrintUser($user);
+        self::PrintNotes($user["uniqueID"]);
         
         self::Finish($res);
     }
     
     public static function PrintUser($user) {
         echo "<section id=\"user\">\n";
-        echo "<h1>".$user["email"]."</h1>\n";
+        echo "<h1>".$user["email"]." (Unique ID #".$user["uniqueID"].")</h1>\n";
         echo "<table>\n";
         echo "<tr><td>Update Transaction ID</td><td>".$user["updateSeqID"]."</td></tr>\n";
         echo "<tr><td>Delete Transaction ID</td><td>".$user["deleteSeqID"]."</td></tr>\n";
+         echo "</table>\n";
         echo "</section>\n";
     }
     
     public static function PrintNotes($userID) {
         
+        $stmt = \app::Connection()->prepare("SELECT * FROM `note` WHERE userID = :userID");
+        if(!$stmt->execute(["userID" => $userID])) {
+            return;
+        }
+        
+        echo "<section id=\"notes\">\n";
+        echo "<h1>".$user["email"]."</h1>\n";
+        echo "<table>\n";
+        echo "<tr><th>ID</th><th>Contents</th></tr>\n";
+        while($note = $stmt->fetch(\PDO::FETCH_ASSOC)) {
+            echo "<tr>\n";
+            echo "<td>".$note["uniqueID"]."</td>";
+            echo "<td>".$note["text"]."</td>";
+            echo "</tr>\n";
+        }
+        echo "</table>\n";
+        echo "</section>\n";
+    }
+    
+    public static function Start() {
+        ob_start();
+        echo self::$header;
     }
     
     public static function Finish($res) {
-        $res->SetHeader("Content-Type", "text/html");
+        echo self::$footer;
         $out = ob_get_clean();
         $res->SetBody($out);
+        $res->SetHeader("Content-Type", "text/html");
     }
     
     private static function GetUser($email) {
